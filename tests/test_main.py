b831c9339cd9a194a4f92c1588c84196e1ba1fa8
Saed Nyarko <saed.nyarko@vennote.com>
2024-11-16 01:01:35 +0000

    Update the tests/test_main.py file

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,10 +2,11 @@
 This module contains tests for the main application code.
 """
 
-from app.app import hello_world
+from app import hello_world
 
 def test_hello_world():
     """
     Test the hello_world function.
     """
     assert hello_world() == "Hello, world!"
+    
